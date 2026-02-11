1b99dc1e5124677ea9ef9a31d542d920091f43b4
Rela82me <rela82me@gmail.com>
2026-02-10 22:17:29 -0500

    adding silver layer

--- scripts/Silver/ddl_silver.sql
+++ scripts/Silver/ddl_silver.sql
@@ -10,13 +10,13 @@ Run this script to redefine the DDL structure of the silver tables.
 IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info (
-    cst_id INT,
+    cst_id				INT,
     cst_key             NVARCHAR(50),
     cst_firstname       NVARCHAR(50),
     cst_lastname        NVARCHAR(50),
-    cst_material_status NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
     cst_gndr            NVARCHAR(50),
-    cst_created_date    DATE,
+    cst_create_date		DATE,
     dwh_create_date     DATETIME2 DEFAULT GETDATE()
 
 );
@@ -25,12 +25,13 @@ IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info (
 	prd_id              INT,
+	cat_id				NVARCHAR(50),
 	prd_key             NVARCHAR(50),
 	prd_nm              NVARCHAR(50),
 	prd_cost            INT,
 	prd_line            NVARCHAR(50),
-	prd_start_dt        DATETIME,
-	prd_end_dt          DATETIME,
+	prd_start_dt        DATE,
+	prd_end_dt          DATE,
     dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 
@@ -40,9 +41,9 @@ CREATE TABLE silver.crm_sales_details (
     sls_ord_num         NVARCHAR(50),
     sls_prd_key         NVARCHAR(50),
     sls_cust_id         INT,
-    sls_order_dt        INT,
-    sls_ship_dt         INT,
-    sls_due_dt          INT,
+    sls_order_dt        DATE,
+    sls_ship_dt         DATE,
+    sls_due_dt          DATE,
     sls_sales           INT,
     sls_quantity        INT,
     sls_price           INT,
